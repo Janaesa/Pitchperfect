d935ae9ba985877418b46d6ab8cd90bd4b4904fa
JOELLE_STRESSLER <JES21N@FSU.EDU>
2024-10-02 10:13:46 -0400

    GUI Start

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let gui;
+let x;
 let clouds;
 
 function preload(){
@@ -8,12 +10,26 @@ function setup() {
      //Creates a canvas that is 600 pixels wide
     //and 400 pixels high
      createCanvas(1000, 1000);
+     gui = createGui();
+     x = createSliderV("Slider",50,50);
 
     }
+
+
     function draw() {
         //sky background
       background(205, 240, 255);
       image(clouds, 40, 50);
       image(clouds, 300,30);
       image(clouds, 600,5);
+      drawGui();
+
+      if(x.isChanged) {
+        print(x.label + " = " + x.val);
+      }
+      
+    }
+
+    function touchMoved() {
+      return false;
     }
