2de434c1b47ae290b6296111bb916ef2bb44fe43
JOELLE_STRESSLER <JES21N@FSU.EDU>
2024-10-02 10:07:17 -0400

    More Clouds

--- sketch.js
+++ sketch.js
@@ -1,15 +1,19 @@
 let clouds;
 
+function preload(){
+  clouds = loadImage("assets/clouds.png");
+}
+
 function setup() {
      //Creates a canvas that is 600 pixels wide
     //and 400 pixels high
      createCanvas(1000, 1000);
 
-     cloud = loadImage("assets/clouds.png");
-
     }
     function draw() {
         //sky background
       background(205, 240, 255);
-      image(cloud, 40, 50);
+      image(clouds, 40, 50);
+      image(clouds, 300,30);
+      image(clouds, 600,5);
     }
